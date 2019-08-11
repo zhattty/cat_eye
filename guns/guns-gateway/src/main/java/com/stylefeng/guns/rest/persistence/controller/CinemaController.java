26dae54fba4ab7078408092be3150b56e2676c59
amazineHu <2716246105@qq.com>
2019-08-11 15:48:52 +0800

    测试冲突

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/persistence/controller/CinemaController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/persistence/controller/CinemaController.java
@@ -149,6 +149,8 @@ public class CinemaController {
                 map2.put("filmInfo", filmInfo);
                 map2.put("cinemaInfo", cinemaInfo);
                 map2.put("hallInfo", hallInfo);
+                map2.put("soldSeats", soldSeats);
+
 
                 map1.put("status", 0);
                 map1.put("imgPre", "http://img.meetingshop.cn/");
